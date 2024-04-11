b10d510e44c1445ef8f814fb2a9cd86f5efed941
ZTGP_VR <popov-mp@mail.ru>
2024-04-11 17:30:25 +0500

    готовый вариант без отличия текста от цифр

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,24 +5,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        int x = 1;
+        int x = 30;
         int n = 1;
-        //boolean exit;
-        //exit = console.nextLine().equalsIgnoreCase("exit");
-        System.out.println("Введите имя 1 участника. Или exit");
-        boolean exit;
-        exit = console.nextLine().equalsIgnoreCase("exit");
-        while (console.hasNextLine()) {
-            String[] name = new String[100];
-            if (!exit) {
-                name[x] = console.nextLine();
-                x++;
-                System.out.println("Введите имя " + x + " участника. Или exit");
-            } else break;
-            System.out.println(name[1]);
+        boolean exit = false;
+        String[] name = new String[x];
+        while (!exit) {
+            System.out.println("Введите имя " + n + " участника или exit");
+            name[x - 1] = console.nextLine();
+            if (name[x - 1].equalsIgnoreCase("exit")) {
+                break;
+            }
+            x--;
+            n++;
         }
-
-
+        System.out.println(x);
+        System.out.println(n);
+        while (n > 1) {
+            x++;
+            n--;
+        }
     }
 }
+
+
+
 
