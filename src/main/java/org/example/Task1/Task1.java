d4f32e8d818baaf264441f30e6f140e737804111
ZTGP_VR <popov-mp@mail.ru>
2024-04-11 20:14:53 +0500

    комит 4 меняю условие цикла while

--- src/main/java/org/example/Task1/Task1.java
+++ src/main/java/org/example/Task1/Task1.java
@@ -1,13 +1,14 @@
 package org.example.Task1;
+
 import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         int x = Short.MAX_VALUE; //переменная длины массива
         int n = 1; //переменная кол-ва имен
-        boolean exit = false;
         String[] name = new String[x + 1];
-        while (!exit) {
+        while (true) {
             System.out.println("Введите имя " + n + " участника или exit");
             name[x] = console.nextLine();
             if (name[x].equalsIgnoreCase("exit")) {
