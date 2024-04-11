671d03bfe195e4da5b0c71d3253dbe0b00c214bf
ZTGP_VR <popov-mp@mail.ru>
2024-04-11 14:57:23 +0500

    4 вариант

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner console = new Scanner(System.in);
+        int x = 1;
+        int n = 1;
+        //boolean exit;
+        //exit = console.nextLine().equalsIgnoreCase("exit");
+        System.out.println("Введите имя 1 участника. Или exit");
+        boolean exit;
+        exit = console.nextLine().equalsIgnoreCase("exit");
+        while (console.hasNextLine()) {
+            String[] name = new String[100];
+            if (!exit) {
+                name[x] = console.nextLine();
+                x++;
+                System.out.println("Введите имя " + x + " участника. Или exit");
+            } else break;
+            System.out.println(name[1]);
+        }
+
+
     }
 }
+
